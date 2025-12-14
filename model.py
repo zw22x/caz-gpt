96450ede5e72c10cc20ee04cb6349d39d5fb2293
Zac Westbrook <zacwestbrook24@gmail.com>
2025-12-14 00:40:34 -0500

    a basic 124M gpt model, still a lot of work and learning to do, but i am enjoying it

--- model.py
+++ model.py
@@ -77,7 +77,7 @@ class GPTConfig:
     block_size: int = 1024
     vocab_size: int = 50304
     n_layer: int = 12
-    n_head: int 12
+    n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorm(gpt2), False: a bit better and faster
