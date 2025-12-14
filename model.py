24ab4a99840b2d042ba916b5cfdfc253c34606e7
William Westbrook <williamwestbrook@Williams-MacBook-Pro.local>
2025-12-14 00:10:11 -0500

    finished a base 124M model, custom LayerNorm, FlashAttention, need to add a few more functions, now to build our training stack

--- model.py
+++ model.py
@@ -35,4 +35,121 @@ def CausalSelfAttention(nn.Module):
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        # TODO: FlashAttention 2 via PyTorch
+        # FlashAttention 2 via PyTorch
+        att = F.scaled_dot_product_attention(
+            q, k, v, 
+            dropout_p=self.dropout if self.training else 0.0:
+            is_casual=True
+        )
+        att = att.transpose(1, 2).contiguous().view(B, T, C)
+        return self.resid_dropout(self.c_proj(att))
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+    
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+    
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50304
+    n_layer: int = 12
+    n_head: int 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True # True: bias in Linears and LayerNorm(gpt2), False: a bit better and faster
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), 
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        ))
+        self.lm_head = nn.linear(config.n_embd, config.vocab_size, bias=False)
+        # 'UserWarning' functional_call was passed multiple calues for tied weights
+        self.transformer.wte.weight = self.lm_head.weight # # https://paperswithcode.com/method/weight-tying
+
+        # initialize all weights
+        self.apply(self._init_weights)
+        # apply special scaled init to residual prjections, gpt-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layyer))
+        
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        # return number of parameters in the model
+        # return non-embedding count, poition of embeddings get subtracted
+        # token embeddings normally do too but because of paramter sharaing
+        # these are used as weights in final layer, include them
+        n_params = sum(p.num3l() for p in self.parameters())
+        if non_embedding: 
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.Embedding):
+                torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        device = idx.device 
+        b, t = idx.size()
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+        
+        # forward GPT model itself
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
+        x = self.transformer.drop(tok_emb + pos_emb)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        else: 
+            # inference-time mini-optimization/ only forward lm_head on the very last position
+            logits = self.lm_head(x[:, [-1], :]) # using list [-1] to preserve the time dim
+            loss = None
+
+# TODO: crop_block_size function, from_pretrained function, optimizer configuration function
+# TODO: estimate_mfu, generate
